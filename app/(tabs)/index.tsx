1252b6f7ee776e5d562b60d6cb6cf812e8b8e0a3
sergiocasallas <sergiocasallas@pop-os.localdomain>
2024-05-18 20:59:43 -0500

    feat: configuracion maps y api

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,43 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from "react-native";
+import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
 
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
+import { View } from "@/components/Themed";
 
 export default function TabOneScreen() {
+  const INITIAL_REGION = {
+    latitude: 4.710989,
+    longitude: -74.07209,
+    latitudeDelta: 2,
+    longitudeDelta: 2,
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      {/* <MapView /> */}
+      <MapView
+        style={{ width: "100%", height: "100%" }}
+        provider={PROVIDER_GOOGLE}
+        region={{
+          latitude: 4.710989,
+          longitude: -74.07209,
+          latitudeDelta: 2,
+          longitudeDelta: 2,
+        }}
+        initialRegion={INITIAL_REGION}
+        showsUserLocation={true}
+        showsMyLocationButton={true}
+        showsCompass={true}
+        toolbarEnabled={true}
+        zoomEnabled={true}
+        rotateEnabled={true}
+        // onMapReady={() => {
+        //   PermissionsAndroid.request(
+        //     PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION
+        //   ).then((granted) => {
+        //     alert(granted); // just to ensure that permissions were granted
+        //   });
+        // }}
+      />
     </View>
   );
 }
@@ -16,16 +45,16 @@ export default function TabOneScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
   },
 });
